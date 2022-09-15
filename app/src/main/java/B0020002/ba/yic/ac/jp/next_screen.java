c320e56626cf2ca9646db5f05e9ed1579c4f2883
root <root@PB0020002.localdomain>
2022-09-15 09:59:33 +0900

    Initial commit

---
+++ app/src/main/java/B0020002/ba/yic/ac/jp/next_screen.java
@@ -0,0 +1,15 @@
+package B0020002.ba.yic.ac.jp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class next_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.next_screen);
+    }
+}
